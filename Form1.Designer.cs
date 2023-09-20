5251fc34a4d608b2a5e92000fbd1ff24edbb5d6e
GUHhRD <124718091+GUHhRD@users.noreply.github.com>
2023-09-20 00:17:26 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Woofer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneMaterialTextBox1 = new Siticone.UI.WinForms.SiticoneMaterialTextBox();
+            this.siticoneMaterialTextBox2 = new Siticone.UI.WinForms.SiticoneMaterialTextBox();
+            this.siticoneButton2 = new Siticone.UI.WinForms.SiticoneButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(191, 277);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton1.TabIndex = 0;
+            this.siticoneButton1.Text = "siticoneButton1";
+            // 
+            // siticoneMaterialTextBox1
+            // 
+            this.siticoneMaterialTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneMaterialTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneMaterialTextBox1.DefaultText = "";
+            this.siticoneMaterialTextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneMaterialTextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneMaterialTextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneMaterialTextBox1.DisabledState.Parent = this.siticoneMaterialTextBox1;
+            this.siticoneMaterialTextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneMaterialTextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneMaterialTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneMaterialTextBox1.FocusedState.Parent = this.siticoneMaterialTextBox1;
+            this.siticoneMaterialTextBox1.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneMaterialTextBox1.HoveredState.Parent = this.siticoneMaterialTextBox1;
+            this.siticoneMaterialTextBox1.Location = new System.Drawing.Point(157, 106);
+            this.siticoneMaterialTextBox1.Name = "siticoneMaterialTextBox1";
+            this.siticoneMaterialTextBox1.PasswordChar = '\0';
+            this.siticoneMaterialTextBox1.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneMaterialTextBox1.PlaceholderText = "";
+            this.siticoneMaterialTextBox1.SelectedText = "";
+            this.siticoneMaterialTextBox1.ShadowDecoration.Parent = this.siticoneMaterialTextBox1;
+            this.siticoneMaterialTextBox1.Size = new System.Drawing.Size(290, 40);
+            this.siticoneMaterialTextBox1.TabIndex = 1;
+            // 
+            // siticoneMaterialTextBox2
+            // 
+            this.siticoneMaterialTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneMaterialTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneMaterialTextBox2.DefaultText = "";
+            this.siticoneMaterialTextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneMaterialTextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneMaterialTextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneMaterialTextBox2.DisabledState.Parent = this.siticoneMaterialTextBox2;
+            this.siticoneMaterialTextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneMaterialTextBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneMaterialTextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneMaterialTextBox2.FocusedState.Parent = this.siticoneMaterialTextBox2;
+            this.siticoneMaterialTextBox2.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneMaterialTextBox2.HoveredState.Parent = this.siticoneMaterialTextBox2;
+            this.siticoneMaterialTextBox2.Location = new System.Drawing.Point(157, 190);
+            this.siticoneMaterialTextBox2.Name = "siticoneMaterialTextBox2";
+            this.siticoneMaterialTextBox2.PasswordChar = '\0';
+            this.siticoneMaterialTextBox2.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneMaterialTextBox2.PlaceholderText = "";
+            this.siticoneMaterialTextBox2.SelectedText = "";
+            this.siticoneMaterialTextBox2.ShadowDecoration.Parent = this.siticoneMaterialTextBox2;
+            this.siticoneMaterialTextBox2.Size = new System.Drawing.Size(290, 40);
+            this.siticoneMaterialTextBox2.TabIndex = 2;
+            // 
+            // siticoneButton2
+            // 
+            this.siticoneButton2.CheckedState.Parent = this.siticoneButton2;
+            this.siticoneButton2.CustomImages.Parent = this.siticoneButton2;
+            this.siticoneButton2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton2.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton2.HoveredState.Parent = this.siticoneButton2;
+            this.siticoneButton2.Location = new System.Drawing.Point(191, 341);
+            this.siticoneButton2.Name = "siticoneButton2";
+            this.siticoneButton2.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.siticoneButton2.ShadowDecoration.Parent = this.siticoneButton2;
+            this.siticoneButton2.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton2.TabIndex = 3;
+            this.siticoneButton2.Text = "siticoneButton2";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(625, 32);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.ClientSize = new System.Drawing.Size(624, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.siticoneButton2);
+            this.Controls.Add(this.siticoneMaterialTextBox2);
+            this.Controls.Add(this.siticoneMaterialTextBox1);
+            this.Controls.Add(this.siticoneButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+        private Siticone.UI.WinForms.SiticoneMaterialTextBox siticoneMaterialTextBox1;
+        private Siticone.UI.WinForms.SiticoneMaterialTextBox siticoneMaterialTextBox2;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
